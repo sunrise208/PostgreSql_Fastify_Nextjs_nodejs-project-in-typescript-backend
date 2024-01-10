72d48cb802d1bd439800c28f39d1bd4c63cdeeca
sunrise <sunrise96208@gamil.com>
2024-01-09 23:55:55 -0800

    authRoute/test completed...

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const { Client, Pool } = require('pg');
+const Fastify = require("fastify");
+const authRoute = require("./routes/auth");
+const dbConnector = require("./dbconnect");
 
-const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'sunrise',
-    password: 'rlafuaud',
-    port: 5432,
-})
+const fastify = Fastify({
+  //   logger: true,
+});
 
-client.connect().then(() => console.log("PostgreSQL is connected..."))
+fastify.register(authRoute, { prefix: "/auth" });
+fastify.register(dbConnector);
+
+try {
+  fastify.listen({ port: 5000 }, () =>
+    console.log("fastify server is running on port 5000...")
+  );
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
