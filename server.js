331f9d1563d40435e3e427b8002ca3f1920cd050
sunrise <sunrise96208@gamil.com>
2024-01-10 23:14:24 -0800

    resgier operation complete

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Fastify = require("fastify");
 const { Client } = require("pg");
 const bcrypt = require("bcryptjs");
+const fastifyCors = require("@fastify/cors");
 
 const fastifyFormbody = require("./fastifyFormBody");
 
@@ -16,19 +17,30 @@ const client = new Client({
   port: 5432,
 });
 client.connect().then(() => console.log("postgreSql is connected..."));
+fastify.register(fastifyCors, { origin: "*" });
 
 fastify.post("/auth/register", async function (req, reply) {
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash(req.body.password, salt, function (err, hash) {
-      client.query(
-        "INSERT INTO users.users (name, email, password) VALUES($1, $2, $3) RETURNING '*'",
-        [req.body.name, req.body.email, hash],
-        function onResult(err, result) {
-          reply.send(err || result);
-        }
-      );
-    });
-  });
+  client.query(
+    "select email from users.users where email=$1",
+    [req.body.email],
+    function onResult(err, result) {
+      if (!result.rows.length) {
+        bcrypt.genSalt(10, function (err, salt) {
+          bcrypt.hash(req.body.password, salt, function (err, hash) {
+            client.query(
+              "INSERT INTO users.users (name, email, password) VALUES($1, $2, $3) RETURNING '*'",
+              [req.body.name, req.body.email, hash],
+              function onResult(err, result) {
+                reply.send(err || result);
+              }
+            );
+          });
+        });
+      } else {
+        console.log({ msg: "email already exists..." });
+      }
+    }
+  );
 });
 
 try {
